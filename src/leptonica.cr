45293407bfa203a8c48ea6fc270045a6ddc365b7
Jack Thorne <jack@kilmer.co>
2022-12-12 13:43:59 -0600

    expose basic init

--- src/leptonica.cr
+++ src/leptonica.cr
@@ -2,4 +2,36 @@ require "./lib_leptonica"
 
 module Leptonica
   VERSION = "0.1.0"
+
+  class Pix
+    @handle : LibLeptonica::Pix*
+
+    def initialize(path : String)
+      img = LibLeptonica.pix_read(path)
+
+      if img == nil
+        raise "unable to read image"
+      else
+        @handle = img
+      end
+    end
+
+    def finalize
+      LibLeptonica.pix_destroy(@handle)
+    end
+
+    def to_unsafe
+      @handle
+    end
+
+    def dimentions
+      w = uninitialized Int32
+      h = uninitialized Int32
+      d = uninitialized Int32
+
+      LibLeptonica.get_dimensions(self, pointerof(w), pointerof(h), pointerof(d))
+
+      {w, h, d}
+    end
+  end
 end
--- src/lib_leptonica.cr
+++ src/lib_leptonica.cr
@@ -1,7 +1,14 @@
 @[Link("leptonica")]
 lib LibLeptonica
-  type Pix = Void*
+  MAJOR_VERSION =  1
+  MINOR_VERSION = 82
+  PATCH_VERSION =  0
 
-  fun pix_read = pixRead(outputbase : LibC::Char*) : Pix
+  type Pix = Void*
+  type LInt32 = Int32
+
+  fun pix_read = pixRead(outputbase : LibC::Char*) : Pix*
   fun pix_destroy = pixDestroy(ppix : Pix*)
+  # fun int32 = l_int32(x : Int32) : LInt32
+  fun get_dimensions = pixGetDimensions(pix : Pix*, pw : Int32*, ph : Int32*, pd : Int32*)
 end
